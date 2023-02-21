7e1ff0d56b5364127e082f34f1b3c44366f8d845
Selvabharathi7 <125946097+Selvabharathi7@users.noreply.github.com>
2023-02-21 16:33:31 +0530

    Updated Naming Conventions

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#Title              : Online Food Ordering System 
+#Author             : Selvabharathi CT 
+#Created on         :  08.02.2023 
+#Last Modified Date : 21.02.2023 
+#Reviewed by        : Shilpa M 
+#Reviewed on        : 21.02.2023 
+
 from Login import * 
 
 login(username,password)
